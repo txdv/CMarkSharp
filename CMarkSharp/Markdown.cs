a9a2427d81152ab389a07c8acbd0d82c6c083941
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-06-25 12:02:15 +0200

    Expose first function: IntegerVersion
    returns the interger of the version

---
+++ CMarkSharp/Markdown.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CMarkSharp
+{
+	public class Markdown
+	{
+		[DllImport("cmark", CallingConvention=CallingConvention.Cdecl)]
+		internal static extern int cmark_version();
+
+		public static int IntegerVersion {
+			get {
+				return cmark_version();
+			}
+		}
+	}
+}
+
